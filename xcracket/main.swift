9a3a07bf6f8825602df04953978e776c32425bc9
Noah Peeters <noah.peeters@icloud.com>
2018-01-10 23:22:02 +0100

    Added v1.0

--- xcracket/main.swift
+++ xcracket/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+guard CommandLine.arguments.count == 2 else {
+    print("xcracket: Wrong usage")
+    exit(1)
+}
 
+let process = Process()
+process.launchPath = "/usr/local/bin/raco"
+process.arguments = ["exe", CommandLine.arguments[1]]
+
+let pipe = Pipe()
+process.standardError = pipe
+process.launch()
+
+guard let input = String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8) else {
+    print("xcracket: Invalid Input")
+    exit(1)
+}
+
+//let regexPattern = " *(.*\\.rkt):(\\d+):(\\d+): (.*)\n.*context...:\n *?(.*)\n"
+let regexPattern = " *(.*?\\.rkt):(\\d+):(\\d+): (.*)\n * context"
+let regex = try! NSRegularExpression(pattern: regexPattern, options: .dotMatchesLineSeparators)
+let matches = regex.matches(in: input, options: [], range: NSRange(input.startIndex..<input.endIndex, in: input))
+
+
+
+for match in matches {
+    let fileName = input[Range(match.range(at: 1), in: input)!]
+    let row = input[Range(match.range(at: 2), in: input)!]
+    let column = input[Range(match.range(at: 3), in: input)!]
+    let message = input[Range(match.range(at: 4), in: input)!]
+    print("\(fileName):\(row):\(column): \(message.replacingOccurrences(of: "\n", with: ""))")
+}
+
+if matches.count != 0 {
+    exit(1)
+}
