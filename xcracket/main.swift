5a60a194fbb3498355753a9d5dde010c2e563e5d
Noah Peeters <noah.peeters@icloud.com>
2018-01-10 21:58:18 +0100

    Init

---
+++ xcracket/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xcracket
+//
+//  Created by Noah Peeters on 10.01.18.
+//  Copyright © 2018 Peeters. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
